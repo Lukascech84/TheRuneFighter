ea11df740ed89986ea17805b4c5a735ecb371551
Lukascech84 <it2105@sspu-opava.cz>
2024-11-05 22:50:16 +0100

    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,125 +1,127 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    private Vector2 move, mouseLook, joystickLook;
-    private Vector3 rotationTarget, playerVelocity;
+    [Header("Movement Settings")]
+    public float speed = 5.0f;
+    public float gravity = -9.81f;
+    public float jumpSpeed = 2.0f;
     public bool isPc;
     public CharacterController controller;
-    public float gravity = -9.8f;
 
+    [Header("Rotation Settings")]
+    [SerializeField] private float rotationSmoothTime = 0.15f;
 
+    private Vector2 move, mouseLook, joystickLook;
+    private Vector3 rotationTarget, playerVelocity;
+    private bool isJumping;
+    private Camera mainCamera;
 
-    public void OnMove(InputAction.CallbackContext context)
+    private void Awake()
     {
-        move = context.ReadValue<Vector2>();
+        // Cache main camera for performance
+        mainCamera = Camera.main;
     }
+
+    // Input callbacks
+    public void OnMove(InputAction.CallbackContext context) => move = context.ReadValue<Vector2>().normalized;
 
     public void OnMouseLook(InputAction.CallbackContext context)
-    {
-        mouseLook = context.ReadValue<Vector2>();
-    }
+        => mouseLook = Vector2.ClampMagnitude(context.ReadValue<Vector2>(), 1f);
 
     public void OnJoystickLook(InputAction.CallbackContext context)
+        => joystickLook = Vector2.ClampMagnitude(context.ReadValue<Vector2>(), 1f);
+
+    public void OnJump(InputAction.CallbackContext context)
     {
-        joystickLook = context.ReadValue<Vector2>();
+        if (context.performed && controller.isGrounded)
+        {
+            isJumping = true;
+        }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
+        ApplyGravity();
+        if (isPc) HandleMouseLook();
+        else HandleJoystickLook();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ApplyGravity()
     {
-        // Gravitace
-        playerVelocity.y += gravity * Time.deltaTime;
-
         if (controller.isGrounded && playerVelocity.y < 0)
         {
-            playerVelocity.y = -2f;
+            playerVelocity.y = -2f; // Small downward force for consistent grounded detection
         }
-
+        else
+        {
+            playerVelocity.y += gravity * Time.deltaTime;
+        }
         controller.Move(playerVelocity * Time.deltaTime);
+    }
 
+    private void HandleMouseLook()
+    {
+        if (mainCamera == null) return;
 
-        if (isPc)
+        Ray ray = mainCamera.ScreenPointToRay(mouseLook);
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(mouseLook);
+            rotationTarget = hit.point;
+        }
+        MoveWithAim(rotationTarget);
+    }
 
-            if(Physics.Raycast(ray, out hit))
-            {
-                rotationTarget = hit.point;
-            }
-
-            movePlayerWithAim();
-
+    private void HandleJoystickLook()
+    {
+        if (joystickLook.sqrMagnitude < 0.1f)
+        {
+            MoveWithoutAim();
         }
         else
         {
-            if(joystickLook.x == 0 && joystickLook.y == 0)
-            {
-                movePlayer();
-            }
-            else
-            {
-                movePlayerWithAim();
-            }
+            Vector3 aimDirection = transform.position + new Vector3(joystickLook.x, 0, joystickLook.y);
+            MoveWithAim(aimDirection);
         }
-       
     }
 
-    public void movePlayer()
+    private void MoveWithoutAim()
     {
         Vector3 movement = new Vector3(move.x, 0f, move.y);
-
-        if(movement != Vector3.zero)
+        if (movement.sqrMagnitude > 0.01f)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSmoothTime);
         }
-
-
-        controller.Move(movement * speed * Time.deltaTime);
-
+        ApplyMovement(movement);
     }
 
-    public void movePlayerWithAim()
+    private void MoveWithAim(Vector3 aimPoint)
     {
-        if(isPc)
+        Vector3 lookDirection = aimPoint - transform.position;
+        lookDirection.y = 0;
+
+        if (lookDirection.sqrMagnitude > 0.01f)
         {
-            var lookPos = rotationTarget - transform.position;
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-
-            Vector3 aimDirection = new Vector3(rotationTarget.x, 0f, rotationTarget.z);
-
-            if(aimDirection != Vector3.zero)
-            {
-                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
-            }
-        }
-        else
-        {
-            Vector3 aimDirection = new Vector3(joystickLook.x, 0f, joystickLook.y);
-
-            if (aimDirection != Vector3.zero)
-            {
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(aimDirection), 0.15f);
-            }
+            Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSmoothTime);
         }
 
-        Vector3 movement = new Vector3(move.x, 0f, move.y);
-
-        controller.Move(movement * speed * Time.deltaTime);
-
-
+        Vector3 movement = new Vector3(move.x, 0, move.y);
+        ApplyMovement(movement);
     }
 
+    private void ApplyMovement(Vector3 movement)
+    {
+        if (isJumping && controller.isGrounded)
+        {
+            playerVelocity.y = jumpSpeed;
+            isJumping = false;
+        }
+
+        movement *= speed;
+        movement.y = playerVelocity.y;  // Maintain vertical velocity from gravity or jump
+        controller.Move(movement * Time.deltaTime);
+    }
 }
