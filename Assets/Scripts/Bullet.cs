5392368cc4b2ea072626e4da955a27d70331b8ff
Lukascech84 <it2105@sspu-opava.cz>
2024-12-04 15:35:55 +0100

    FlashRed when hit

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -13,15 +13,15 @@ public class Bullet : MonoBehaviour
     public GameObject spawner;
     public AttributeManager playerAtm;
     public AttributeManager enemyAtm;
+
     public GameObject muzzlePrefab;
     public GameObject hitPrefab;
 
     void Start()
     {
-        // Uložíme poèáteèní bod støely
         spawnPoint = transform.position;
 
-        if(muzzlePrefab != null)
+        if (muzzlePrefab != null)
         {
             var muzzleVFX = Instantiate(muzzlePrefab, transform.position, Quaternion.identity);
             muzzleVFX.transform.forward = gameObject.transform.forward;
@@ -30,25 +30,20 @@ public class Bullet : MonoBehaviour
 
     void Update()
     {
-        // Znièíme støelu po uplynutí životnosti
         if (timer > bulletLife) Destroy(this.gameObject);
 
-        // Aktualizujeme èasovaè
         timer += Time.deltaTime;
 
-        // Posuneme støelu ve smìru jejího pohybu
         transform.position = Movement(timer);
     }
 
     public void SetDirection(Vector3 dir)
     {
-        // Nastavíme smìr støely (normalizovaný)
         direction = dir.normalized;
     }
 
     private Vector3 Movement(float timer)
     {
-        // Vypoèítáme novou pozici støely na základì smìru a rychlosti
         float x = timer * speed * direction.x;
         float z = timer * speed * direction.z;
         return new Vector3(x + spawnPoint.x, spawnPoint.y, z + spawnPoint.z);
@@ -61,13 +56,8 @@ public class Bullet : MonoBehaviour
             playerAtm = spawner.GetComponent<AttributeManager>();
             enemyAtm = other.GetComponent<AttributeManager>();
 
-            // Pokud kulka zasáhne svùj pùvodce, ignorujeme kolizi
-            if (other.gameObject == spawner)
-            {
-                return;
-            }
+            if (other.gameObject == spawner) return;
 
-            // Zkontrolujeme, zda zasažený objekt má komponentu Health
             if (enemyAtm != null)
             {
                 if (enemyAtm.health > 0f)
@@ -77,13 +67,12 @@ public class Bullet : MonoBehaviour
             }
         }
 
-        // Znièíme kulku po zásahu
-        if (other.CompareTag("Player") || other.CompareTag("room")) return;
+        if (other.CompareTag("room")) return;
 
         if (hitPrefab != null)
         {
             var hitVFX = Instantiate(hitPrefab, transform.position, Quaternion.identity);
-            Destroy(hitVFX, 2f); // Optional: Znièíme efekt po 2 vteøinách
+            Destroy(hitVFX, 2f);
         }
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/FlashRed.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashRed : MonoBehaviour
+{
+
+    public Material flashRedMaterial;
+    public float flashDuration = 0.05f;
+    private new Renderer renderer;
+    private Material originalMat;
+
+    private GameObject spawner;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        originalMat = renderer.material;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        spawner = other.GetComponent<Bullet>().spawner;
+        if (spawner != null) {
+            if (spawner == gameObject) return;
+            StartCoroutine(Flash(flashDuration));
+            }
+        }
+
+
+    private IEnumerator Flash(float duration)
+    {
+        
+
+        if (renderer != null)
+        {
+            
+
+            renderer.material = flashRedMaterial;
+            yield return new WaitForSeconds(duration);
+            renderer.material = originalMat;
+        }
+    }
+}
