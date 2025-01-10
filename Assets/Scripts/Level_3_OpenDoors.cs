cda18f704b37559c1e45bdaf7f72502495637dda
DonyStudios <98156414+DonyStudios@users.noreply.github.com>
2025-01-10 22:55:14 +0100

    vooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolinek

---
+++ Assets/Scripts/Level_3_OpenDoors.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_3_OpenDoors : BaseOpenDoor
+{
+
+
+    public override void OpenDoor()
+    {
+        Doors();
+    }
+
+    private void Doors()
+    {
+        Destroy(this);
+    }
+}
